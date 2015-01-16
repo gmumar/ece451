80153c4c72a8710803a2c86f32145d080f65a316
Umar  Ghulam <umarghul@ug146.eecg>
2015-01-15 19:19:39 -0500

    stuff

---
+++ lab1/full_adder.cmd
@@ -0,0 +1,19 @@
+vector INPUT Cn_1 A B
+ana  Cn_1 A B sum Cn
+stepsize 1000
+setvector INPUT 000
+s
+setvector INPUT 001
+s
+setvector INPUT 010
+s
+setvector INPUT 011
+s
+setvector INPUT 100
+s
+setvector INPUT 101
+s
+setvector INPUT 110
+s
+setvector INPUT 111
+s
